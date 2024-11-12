6eb6bc9282fea4ba95e362ee99ff1d7014cc86ae
AceRb6 <artukano52@gmail.com>
2024-11-12 01:43:03 -0600

    Actualizacion del html del top - Ace

--- MR/API/static/JS/mensaje.js
+++
@@ -1,4 +0,0 @@
-let re=/ab+c/;
-let re1=new RegExp("ab+c");
-alert(re);
-alert(re1);
--- MR/Movies/urls.py
+++ MR/Movies/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from API import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),  # Accede a la vista movie_list
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
